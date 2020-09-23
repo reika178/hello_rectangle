3323010ddc948bcf9191be4a0ec9f5829d715436
reika178 <rechiko178@gmail.com>
2020-09-24 07:22:55 +0900

    task04
    converter_route.dart完成

---
+++ lib/converter_route.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
+import 'package:meta/meta.dart';
+
+import 'package:hello_rectangle/unit.dart';
+
+class ConverterRoute extends StatelessWidget {
+  final List<Unit> units;
+
+  const ConverterRoute({
+    @required this.units,
+  })  : assert(units != null);
+
+  @override
+  Widget build(BuildContext context) {
+    final unitWidget = units.map((Unit unit) {
+      return Container(
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              unit.name,
+              style: Theme.of(context).textTheme.headline,
+            ),
+            Text(
+              'Conversion: ${unit.conversion}',
+              style: Theme.of(context).textTheme.subhead,
+            ),
+          ],
+        ),
+      );
+    }).toList();
+
+    return ListView(
+      children: unitWidgets,
+    );
+  }
+}
