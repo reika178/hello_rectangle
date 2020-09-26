613421816f7888fdda3bd56b404fbb91e2c608c4
reika178 <rechiko178@gmail.com>
2020-09-26 14:20:42 +0900

    task07
    backdrop.dart

--- lib/backdrop.dart
+++ lib/backdrop.dart
@@ -59,4 +59,203 @@ class _BackdropPanel extends StatelessWidget {
     );
   }
 }
-途中！
+class _BackdropTitle extends AnimatedWidget {
+  final Widget frontTitle;
+  final Widget backTitle;
+
+  const _BackdropTitle({
+    Key key,
+    Listenable listenable,
+    this.frontTitle,
+    this.backTitle,
+  })  : super(key: key, listenable: listenable);
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = this.listenable;
+    return DefaultTextStyle(
+      style: Theme.of(context).primaryTextTheme.title,
+      softWrap: false,
+      overflow: TextOverflow.ellipsis,
+      child: Stack(
+        children: <Widget>[
+          Opacity(
+            opacity: CurvedAnimation(
+              parent: ReverseAnimation(animation),
+              curve: Interval(0.5, 1.0),
+              ).value,
+              child: backTitle,
+            ),
+            Opacity(
+              opacity: CurvedAnimation(
+                parent: animation,
+                curve: Interval(0.5, 1.0),
+              ).value,
+              child: frontTitle,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Backdrop extends StatefulWidget {
+  final Category currentCategory;
+  final Widget frontPanel;
+  final Widget backPanel;
+  final Widget frontTitle;
+  final Widget backTitle;
+
+  const Backdrop({
+    @required this.currentCategory,
+    @required this.frontPanel,
+    @required this.backPanel,
+    @required this.frontTitle,
+    @required this.backTitle,
+  })  : assert(currentCategory != null),
+        assert(frontPanel != null),
+        assert(backPanel != null),
+        assert(frontTitle != null),
+        assert(backTitle != null);
+
+  @override
+  _BackdropState createState() => _BackdropState();
+}
+
+class _BackdropState extends State<Backdrop>
+    with SingleTickerProviderStateMixin {
+  final GlobalKey _backdropkey = GlobalKey(debugLabel: 'Backdrop');
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: Duration(milliseconds: 300),
+      value: 1.0,
+      vsync: this,
+    );
+  }
+
+  @override
+  void didUpdateWidget(Backdrop old) {
+    super.didUpdateWidget(old);
+    if (widget.currentCategory != old.currentCategory) {
+      setState(() {
+        _controller.fling(
+          velocity:
+              _backdropPanelVisible ? -_kFlingVelocity : _kFlingVelocity);
+      });
+    } else if (!_backdropPanelVisible) {
+      setState(() {
+        _controller.fling(velocity: _kFlingVelocity);
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  bool get _backdropPanelVisible {
+    final AnimationStatus status = _controller.status;
+    return status = AnimationStatus.completed ||
+        status == AnimationStatus.forward;
+  }
+
+  void _toggleBackdropPanelVisibility() {
+    FocusScope.of(context).requestFocus(FocusNode());
+    _controller.fling(
+      velocity: _backdropPanelVisible ? -_kFlingVelocity : _kFlingVelocity);
+  }
+
+  double get _backdropHeight {
+    final RenderBox renderBox = _backdropKey.currentContext.findRenderObject();
+    return renderBox.size.height;
+  }
+
+  void _handleDragUpdate(DragUpdateDetails details) {
+    if (_controller.isAnimating ||
+        _controller.status == AnimationStatus.completed) return;
+
+    _controller.value -= details.primaryDelta / _backdropHeight;
+  }
+
+  void _handleDragEnd(DragEndDetails details) {
+    if (_controller.isAnimating ||
+        _controller.status == AnimationStatus.completed) return;
+
+    final double flingVelocity =
+        details.velocity.pixelsPerSecond.dy / _backdropHeight;
+    if (flingVelocity < 0.0)
+      _controller.fling(velocity: math.max(_kFlingVelocity, -flingVelocity));
+    else if (flingVelocity > 0.0)
+      _controller.fling(velocity: math.min(-_kFlingVelocity, -flingVelocity));
+    else
+      _controller.fling(
+        velocity:
+            _controller.value < 0.5 ? -_kFlingVelocity : _kFlingVelocity);
+  }
+
+  Widget _buildStack(BuildContext context, BoxConstraints consttainsts) {
+    const double panelTitleHeight = 48.0;
+    final Size panelSize = constraints.biggest;
+    final double panelTop = panelSize.height - panelTitleHeight;
+
+    Animation<RelativeRect> panelAnimation = RelativeRectTween(
+      begin: RelativeRect.fromLTRB(
+        0.0, panelTop, 0.0, panelTop - panelSize.height),
+      end: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
+    ).animate(_controller.view);
+
+    return Container(
+      key: _backdropKey,
+      color: widget.currentCategory.color,
+      child: Stack(
+        children: <Widget>[
+          widget.backPanel,
+          PositionedTransition(
+            rect: panelAnimation,
+            child: _BackdropPanel(
+              onTap: _toggleBackdropPanelVisibility,
+              onVerticalDragUpdate: _handleDragUpdate,
+              onVerticalDragEnd: _handleDragEnd,
+              title: Text(widget.currentCategory.name),
+              child: widget.frontPanel,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: widget.currentCategory.color,
+        elevation: 0.0,
+        leading: IconButton(
+          onPressed: _toggleBackdropPanelVisibility,
+          icon: AnimatedIcon(
+            icon: AnimatedIcons.close_menu,
+            progress: _controller.view,
+          ),
+        ),
+        title: _BackdropTitle(
+          listenable: _controller.view,
+          frontTitle: widget.frontTitle,
+          backTitle: widget.backTitle,
+        ),
+      ),
+      body: LayoutBuilder(
+        builder: _buildStack,
+      ),
+      resizeToAvoidBottomPadding: false,
+    );
+  }
+  }
